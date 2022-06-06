81ea888b983957be23037f3ed43c753c6207fec3
elturanshucai <96484306+elturanshucai@users.noreply.github.com>
2022-06-06 15:12:00 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-// const fs = require('fs');
+const fs = require('fs');
 
-const fehleler=[
-    {id: 1, ad:"Elturan", yash: 20},
-    {id: 2, ad:"Murad", yash: 19},
-    {id: 3, ad:"Ferman", yash: 20}
-]
-
-module.exports={
-    fehleler
-}
-
-// fs.readFile('./text.json', 'utf8', (err, data) => {
-//     if(err){
-//         console.log('xeta')
-//     }
-//     else{
-//         let newdata=JSON.parse(data)
-//         let ages=newdata.map(item=>item.age)
-//         let sum=0
-//         ages.map(item=>sum+=Math.round(parseInt(item)/ages.length))
-//         fs.writeFile('./text.text', String(sum), (err)=>{
-//             if(!err){
-//                 console.log('tamamlandi')
-//             }
-//             }
-//         )}
-// });
+fs.readFile('./text.json', 'utf8', (err, data) => {
+    if(err){
+        console.log('xeta')
+    }
+    else{
+        let newdata=JSON.parse(data)
+        let ages=newdata.map(item=>item.age)
+        let sum=0
+        ages.map(item=>sum+=Math.round(parseInt(item)/ages.length))
+        fs.writeFile('./text.text', String(sum), (err)=>{
+            if(!err){
+                console.log('tamamlandi')
+            }
+            }
+        )}
+});
--- server.js
+++ server.js
@@ -1,17 +1,56 @@
-
-
-
-// import express from ('express');
 const express = require("express")
-// import {fehleler} from ('./index.js')
-const {fehleler}= require('./index.js')
+const fs = require('fs');
 const app = express();
+const data=[
+    {id:1, name:"Elturan"},
+    {id:2, name:"Murad"},
+    {id:3, name:"Samxal"}
+]
+const cards=[
+    {id:1, number:"1234564325"},
+    {id:2, number:"5436325645"},
+    {id:3, number:"9766387532"},
+    {id:4, number:"4728148354"}
+]
+
 app.get('/', (req,res)=>{
-    res.send('Bu route homepage ucundur.')
+    res.send('Home')
 })
-app.get('/fehleler', (req,res)=>{
-    res.send(fehleler)
+app.get('/datalar', (req,res)=>{
+    fs.readFile('./isciler.json', 'utf8', (err, data)=>{
+        if(!err){
+            let newData = JSON.parse(data)
+            res.send(JSON.stringify(newData))
+        }
+        else{
+            res.send('Xeta bash verdi')
+        }
+    })
 })
-app.listen(3000, ()=>{
+app.get('/datalar', (req,res)=>{
+    const count = parseInt(req.query.count);
+    const offset = parseInt(req.query.offset);
+    res.send({ datas: data.slice(offset, offset + count) })
+})
+app.get('/datalar/:id', (req,res)=>{
+    let id=req.params.id
+    let newData=data.find(item=> item.id==id)
+    if(newData){
+       return res.json(newData)
+    }
+    res.send('Istifacedi yoxdur')
+})
+app.get('/card', (req,res)=>{
+    res.json(cards)
+})
+app.get('/card/:id', (req,res)=>{
+    let id=req.params.id
+    let newData=cards.find(item=>item.id==id)
+    if(newData){
+        return res.json(newData)
+    }
+    res.send(404)
+})
+app.listen(5000, ()=>{
     console.log('MY SERVER RUNNING IN THE 3000. PORT')
 })
